ff868ade22585f64be69a505348ac61b1e51407d
pineda.juan92 <pineda.juan92@gmail.com>
2021-11-17 18:29:59 -0500

    warnings reviewed for netify deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
-import axios from "axios";
+// import axios from "axios";
 
 import IcecreamList from "./components/IcecreamList";
 
-import { API_URL } from "./constants";
+// import { API_URL } from "./constants";
 import { getIcecreamAxios } from "./api/icecream";
-import { getIcecreamFetch } from "./api/icecream";
+// import { getIcecreamFetch } from "./api/icecream";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
